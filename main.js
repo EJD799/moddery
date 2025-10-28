c9e0b0e72563c77a486b678d71e24eaa40893ade
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-10-28 10:31:29 +0900

    update

---
+++ main.js
@@ -0,0 +1,42 @@
+$("#toolbar").menu();
+$("#tabs").tabs();
+$("#newProjDlg").dialog({
+  position: { my: "center", at: "center", of: $("body") },
+  resizable: false,
+  height: 400,
+  width: 500
+});
+$("#newProjDlg").dialog("close");
+$("#newProjType").selectmenu();
+$("#newProjCancelBtn").button();
+$("#newProjCreateBtn").button();
+$("#addElementDlg").dialog({
+  position: { my: "center", at: "center", of: $("body") },
+  resizable: false,
+  height: 400,
+  width: 500
+});
+$("#addElementDlg").dialog("close");
+$("#addElementType").selectmenu();
+$("#addElementCancelBtn").button();
+$("#addElementAddBtn").button();
+function openNewProjDlg() {
+  $("#newProjDlg").dialog("open");
+}
+function closeNewProjDlg() {
+  $("#newProjDlg").dialog("close");
+}
+function createProject() {
+  closeNewProjDlg();
+  alert($("#newProjNameBox").val() + "\n" + $("#newProjType").val() + "\n" + $("#newProjNamespaceBox").val());
+}
+function openAddElementDlg() {
+  $("#addElementDlg").dialog("open");
+}
+function closeAddElementDlg() {
+  $("#addElementDlg").dialog("close");
+}
+function addElement() {
+  alert("test");
+}
+$('input').addClass("ui-widget ui-widget-content ui-corner-all");

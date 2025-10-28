f1f9a65848e2480e49067b0c77ddc1f849694899
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-10-28 14:34:06 +0900

    test

--- main.js
+++ main.js
@@ -99,3 +99,15 @@ $("#loaderProgress").progressbar({
   value: 50
 });
 $('input').addClass("ui-widget ui-widget-content ui-corner-all");
+
+function addTab() {
+  var label = "Title" || "Tab " + tabCounter,
+    id = "tabs-" + tabCounter,
+    li = $( tabTemplate.replace( /#\{href\}/g, "#" + id ).replace( /#\{label\}/g, label ) ),
+    tabContentHtml = "Content" || "Tab " + tabCounter + " content.";
+ 
+  tabs.find( ".ui-tabs-nav" ).append( li );
+  tabs.append( "<div id='" + id + "'><p>" + tabContentHtml + "</p></div>" );
+  tabs.tabs( "refresh" );
+  tabCounter++;
+}

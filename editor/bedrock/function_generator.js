002b431e0b9e894a235be66588ac415b1c0e78ce
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-10-30 10:00:17 +0900

    bugfix

--- editor/bedrock/function_generator.js
+++ editor/bedrock/function_generator.js
@@ -2,17 +2,17 @@
 Blockly.BedrockFunction = new Blockly.Generator('BedrockFunction');
 
 // Optional: define order constants (not always needed)
-Blockly.MyLang.ORDER_ATOMIC = 0;
+Blockly.BedrockFunction.ORDER_ATOMIC = 0;
 
 // Define how each block generates code
-Blockly.MyLang['message'] = function (block) {
+Blockly.BedrockFunction['message'] = function (block) {
   const player = block.getFieldValue('PLAYER');
   const message = block.getFieldValue('MESSAGE');
   return 'say ' + player + ' ' + message;
 };
 
 // Define workspaceToCode() helper
-Blockly.MyLang.finish = function (code) {
+Blockly.BedrockFunction.finish = function (code) {
   // Add footer or header if desired
   return code;
 };

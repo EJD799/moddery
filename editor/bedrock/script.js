e3e61d044bc9f6401b375397f2d203f7e1c568dc
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-02 08:04:22 +0900

    test

--- editor/bedrock/script.js
+++ editor/bedrock/script.js
@@ -162,7 +162,7 @@ const bedrockScriptToolbox = {
       name: 'Colour',
       colour: '%{BKY_COLOUR_HUE}',
       contents: [
-        { kind: 'block', type: 'colour_picker' },
+        //{ kind: 'block', type: 'colour_picker' },
         { kind: 'block', type: 'colour_random' },
         { kind: 'block', type: 'colour_rgb' },
         { kind: 'block', type: 'colour_blend' }

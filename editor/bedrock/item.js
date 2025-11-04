5ec75b1010cc15da5e66437c90f70aa32926e55c
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-04 17:52:27 +0900

    update

--- editor/bedrock/item.js
+++ editor/bedrock/item.js
@@ -1,2 +1,21 @@
 $("#categoryBox").selectmenu();
+$("#itemTextureBtn").button();
+$("#addComponentBtn").button();
+$("#addComponentType").selectmenu();
+$("#addComponentCancelBtn").button();
+$("#addComponentAddBtn").button();
 $('input').addClass("ui-widget ui-widget-content ui-corner-all");
+
+$("#addComponentDlg").dialog({
+  position: { my: "center", at: "center", of: $("body") },
+  resizable: false,
+  height: 200,
+  width: 500
+});
+$("#addComponentDlg").dialog("close");
+function addComponent() {
+  $("#addComponentDlg").dialog("open");
+}
+function closeAddComponentDlg() {
+  $("#addComponentDlg").dialog("close");
+}

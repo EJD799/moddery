2acad21d5e55e51f65c89557150b9c8e7ba47a0e
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-04 18:04:46 +0900

    update

--- editor/bedrock/item.js
+++ editor/bedrock/item.js
@@ -4,6 +4,8 @@ $("#addComponentBtn").button();
 $("#addComponentType").selectmenu();
 $("#addComponentCancelBtn").button();
 $("#addComponentAddBtn").button();
+$("#selectTextureCancelBtn").button();
+$("#selectTextureSelectBtn").button();
 $('input').addClass("ui-widget ui-widget-content ui-corner-all");
 
 $("#addComponentDlg").dialog({
@@ -13,9 +15,30 @@ $("#addComponentDlg").dialog({
   width: 500
 });
 $("#addComponentDlg").dialog("close");
+$("#selectTextureDlg").dialog({
+  position: { my: "center", at: "center", of: $("body") },
+  resizable: false,
+  height: 200,
+  width: 500
+});
+$("#selectTextureDlg").dialog("close");
 function addComponent() {
   $("#addComponentDlg").dialog("open");
 }
 function closeAddComponentDlg() {
   $("#addComponentDlg").dialog("close");
+}
+function openSelectTextureDlg() {
+  $("#selectTextureDlg").dialog("open");
+}
+function closeSelectTextureDlg() {
+  $("#selectTextureDlg").dialog("close");
+}
+function createComponent() {
+    alert("Coming Soon!");
+    $("#addComponentDlg").dialog("close");
+}
+function selectTexture() {
+    alert("Coming Soon!");
+    $("#selectTextureDlg").dialog("close");
 }

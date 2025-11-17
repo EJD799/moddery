797016e81e6db8a2fbda88efc8f583f5216f5d06
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-17 09:44:15 +0900

    test

---
+++ item_definitions.js
@@ -0,0 +1,74 @@
+const itemDefinitionsCommon = {
+    "minecraft:acacia_boat": {
+        name: "Acacia Boat",
+        texture: "/item/acacia_boat.png"
+    },
+    "minecraft:acacia_chest_boat": {
+        name: "Acacia Boat with Chest",
+        texture: "/item/acacia_chest_boat.png"
+    },
+    "minecraft:armor_stand": {
+        name: "Armor Stand",
+        texture: "/item/armor_stand.png"
+    },
+    "minecraft:bamboo_raft": {
+        name: "Bamboo Raft",
+        texture: "/item/bamboo_raft.png"
+    },
+    "minecraft:bamboo_chest_raft": {
+        name: "Bamboo Raft with Chest",
+        texture: "/item/bamboo_chest_raft.png"
+    },
+    "minecraft:beetroot_seeds": {
+        name: "Beetroot Seeds",
+        texture: "/item/beetroot_seeds.png"
+    },
+    "minecraft:birch_boat": {
+        name: "Birch Boat",
+        texture: "/item/birch_boat.png"
+    },
+    "minecraft:birch_chest_boat": {
+        name: "Birch Chest Boat",
+        texture: "/item/birch_chest_boat.png"
+    },
+    "minecraft:blue_egg": {
+        name: "Blue Egg",
+        texture: "/item/blue_egg.png"
+    },
+    "minecraft:experience_bottle": {
+        name: "Bottle o' Enchanting",
+        texture: "/item/experience_bottle.png"
+    },
+    "minecraft:bow": {
+        name: "Bow",
+        texture: "/item/bow.png"
+    },
+    "minecraft:brown_egg": {
+        name: "Brown Egg",
+        texture: "/item/brown_egg.png"
+    },
+    "minecraft:bucket": {
+        name: "Bucket",
+        texture: "/item/bucket.png"
+    },
+    "minecraft:axolotl_bucket": {
+        name: "Axolotl Bucket",
+        texture: "/item/axolotl_bucket.png"
+    },
+    "minecraft:cod_bucket": {
+        name: "Cod Bucket",
+        texture: "/item/cod_bucket.png"
+    },
+    "minecraft:pufferfish_bucket": {
+        name: "Pufferfish Bucket",
+        texture: "/item/pufferfish_bucket.png"
+    },
+    "minecraft:salmon_bucket": {
+        name: "Salmon Bucket",
+        texture: "/item/salmon_bucket.png"
+    },
+    "minecraft:tadpole_bucket": {
+        name: "Tadpole Bucket",
+        texture: "/item/tadpole_bucket.png"
+    },
+}

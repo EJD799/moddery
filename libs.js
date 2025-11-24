0d90dabfc1b69786645ba3553ce38aeb036c523c
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-24 15:45:17 +0900

    bugfix

--- libs.js
+++ libs.js
@@ -70,5 +70,5 @@ function beforeLastDot(str) {
 }
 function afterLastDot(str) {
     const i = str.lastIndexOf(".");
-    return i === -1 ? str : str.slice(i, str.length - 1);
+    return i === -1 ? str : str.slice(i + 1, str.length);
 }

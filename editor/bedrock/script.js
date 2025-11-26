fff5f04408e5bfe38e73f8e26e9c76f2a5003d24
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-11-26 17:20:14 +0900

    bugfix

--- editor/bedrock/script.js
+++ editor/bedrock/script.js
@@ -1589,16 +1589,6 @@ Blockly.common.defineBlocks({
     },
 
     updateParameters_: function () {
-      // Save current parameter values
-      const oldValues = [];
-      for (let i = 0; i < this.parameterCount_; i++) {
-        const input = this.getInput("PARAM" + i);
-        if (input) {
-          const textField = input.fieldRow.find(f => f.name && f.name.startsWith("PARAM_NAME_"));
-          oldValues[i] = textField ? textField.getValue() : "name";
-        }
-      }
-
       // Remove old PARAM inputs
       let i = 0;
       while (this.getInput("PARAM" + i)) {
@@ -1606,34 +1596,35 @@ Blockly.common.defineBlocks({
         i++;
       }
 
-      // Rebuild parameter inputs
+      // Add new PARAM inputs before CODE
       for (let i = 0; i < this.parameterCount_; i++) {
+        const removeBtn = new Blockly.FieldLabel("×", undefined, "param-button");
+        removeBtn.CLICKABLE = true;
         const input = this.appendDummyInput("PARAM" + i)
           .appendField("param " + (i + 1))
-          .appendField(
-            new Blockly.FieldTextInput(oldValues[i] || "name"),
-            "PARAM_NAME_" + i
-          )
+          .appendField(new Blockly.FieldTextInput("name"), "PARAM_NAME_" + i)
           .appendField(
             new Blockly.FieldDropdown([
               ["option1", "OPTION1"],
               ["option2", "OPTION2"]
             ]),
             "PARAM_DROPDOWN_" + i
-          );
-
-        // REMOVE BUTTON
-        const removeBtn = new Blockly.FieldLabel("×", undefined, "param-button");
+          )
+          .appendField(removeBtn, "REMOVE_BTN_" + i);
+        
+        console.log(removeBtn.getClickTarget_());
+        //removeBtn.getClickTarget_().addEventListener("mousedown", (e) => {
         removeBtn.getClickTarget_().onclick = (e) => {
+          console.log("remove");
           e.stopPropagation();
           this.parameterCount_--;
           this.updateParameters_();
         };
-        input.appendField(removeBtn, "REMOVE_BTN_" + i);
+        //});
 
         this.moveInputBefore("PARAM" + i, "CODE");
       }
-    }
+    },
   },
 });
 

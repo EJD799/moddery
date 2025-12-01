9dd7ec7990832099bb9e73675c639ff0f5679546
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-12-01 10:57:54 +0900

    test

--- editor/bedrock/script_generator.js
+++ editor/bedrock/script_generator.js
@@ -1,4 +1,4 @@
-Blockly.JavaScript.forBlock['before_event'] = function() {
+Blockly.JavaScript.forBlock['on_start'] = function() {
     return "";
 };
 
@@ -55,5 +55,89 @@ Blockly.JavaScript.forBlock['show_form_var'] = function() {
 };
 
 Blockly.JavaScript.forBlock['form_title'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_body'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_cancel'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_confirm'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_submit'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_button'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_textfield'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_dropdown'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_slider'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_toggle'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_response_action'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_response_message'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_response_modal'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['is_hardcore'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['addobjective'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['removeobjective'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['objectivedisplay'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['hidedisplay'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['operatescore'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['form_slider'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['objective_list'] = function() {
+
+};
+
+Blockly.JavaScript.forBlock['player_list'] = function() {
 
 };

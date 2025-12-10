5c64c8fd1a7f508bf1eeb5fa166686016682788d
EJD799 <108157991+EJD799@users.noreply.github.com>
2025-12-10 11:57:26 +0900

    test

--- libs.js
+++ libs.js
@@ -118,3 +118,33 @@ function getCookie(name) {
 function eraseCookie(name) {   
     document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
+function getMaterialInstances(geoJson) {
+  const results = [];
+
+  // Bedrock models store geometry in "minecraft:geometry"
+  const geometries = geoJson["minecraft:geometry"];
+  if (!Array.isArray(geometries)) return results;
+
+  for (const geometry of geometries) {
+    const bones = geometry.bones;
+    if (!Array.isArray(bones)) continue;
+
+    for (const bone of bones) {
+      // Each bone may have cubes
+      if (Array.isArray(bone.cubes)) {
+        for (const cube of bone.cubes) {
+          if (cube.material_instance) {
+            results.push(cube.material_instance);
+          }
+        }
+      }
+
+      // Optional: If your model uses "poly_mesh" or other elements
+      if (bone.poly_mesh && bone.poly_mesh.material_instance) {
+        results.push(bone.poly_mesh.material_instance);
+      }
+    }
+  }
+
+  return results;
+}

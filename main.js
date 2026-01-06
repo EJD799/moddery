a931ed3ec07ca41ad2905f3c8e0f258bfdc340d6
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-01-06 09:22:44 +0900

    bugfix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const appVersion = "1.1.32";
+const appVersion = "1.1.33";
 const buildDate = "1/5/2026";
 const minEngineVersion = [1, 21, 90];
 const formatVersion = "1.21.90";
@@ -357,7 +357,10 @@ $("#clearCacheBtn").button();
 $("#signInBtn").button();
 $("#themeMenu").selectmenu();
 
-
+$("#optionsContent2").hide();
+$("#optionsContent3").hide();
+$("#optionsContent4").hide();
+$("#optionsContent5").hide();
 function switchOptionsTab(tab) {
   optionsTab1.classList.remove("is-active");
   optionsTab2.classList.remove("is-active");
@@ -370,7 +373,7 @@ function switchOptionsTab(tab) {
   $("#optionsContent4").hide();
   $("#optionsContent5").hide();
   document.getElementById(`optionsTab${tab}`).classList.add("is-active");
-  $(`#optionsContent${tab}`).hide();
+  $(`#optionsContent${tab}`).show();
 }
 function openOptionsDlg() {
   optionsDlg.classList.add("is-active");

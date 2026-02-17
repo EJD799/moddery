2b743181c98b30fbed5d15eaed005d1e95577605
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-02-17 09:08:39 +0900

    bugfix

--- editor/bedrock/biome.js
+++ editor/bedrock/biome.js
@@ -20,15 +20,18 @@ function saveProject() {
 function loadProject(data) {
     elementData = data;
 
+    elementIDBox.value = data.name;
     biomeIDBox.value = data.id;
-    nameBox.value = data.biomeName;
-    dimensionBox.value = data.dimension;
+    nameBox.value = data.biomeName ?? "";
+    dimensionBox.value = data.dimension ?? "Overworld";
 
-    frozenWeight.value = data.locWeight.frozen;
-    coldWeight.value = data.locWeight.cold;
-    mediumWeight.value = data.locWeight.medium;
-    lukewarmWeight.value = data.locWeight.lukewarm;
-    warmWeight.value = data.locWeight.warm;
+    if (data.locWeight) {
+        frozenWeight.value = data.locWeight.frozen;
+        coldWeight.value = data.locWeight.cold;
+        mediumWeight.value = data.locWeight.medium;
+        lukewarmWeight.value = data.locWeight.lukewarm;
+        warmWeight.value = data.locWeight.warm;
+    }
 }
 
 bulmaSelectmenu.attachMenu(dimensionBox);

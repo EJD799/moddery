be3f18064d0cab949a2cb08a513e38300d02b897
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-02-19 08:39:05 +0900

    test

--- editor/bedrock/biome.js
+++ editor/bedrock/biome.js
@@ -1,4 +1,6 @@
 var elementData = {};
+let currentTransformationSelecting;
+let currentTransformations;
 
 
 function saveProject() {
@@ -16,6 +18,7 @@ function saveProject() {
             warm: warmWeight.value,
         },
         tags: tagsBox.value,
+        transformations: currentTransformations,
     };
 }
 function loadProject(data) {
@@ -34,7 +37,75 @@ function loadProject(data) {
         lukewarmWeight.value = data.locWeight.lukewarm;
         warmWeight.value = data.locWeight.warm;
     }
+
+    currentTransformations = data.currentTransformations ?? {
+        hills: "",
+        shore: ""
+    };
+    transformationsKeys = Object.keys(currentTransformations);
+    for (let i = 0; i < transformationsKeys.length; i++) {
+        let selection = currentTransformations[transformationsKeys[i]];
+        if (selection == "None") {
+            currentTransformations[currentTransformationSelecting] = "";
+            textureNameText.innerHTML = "No biome selected";
+        } else {
+            currentTransformations[currentTransformationSelecting] = selection;
+            textureNameText.innerHTML = selection;
+        }
+    }
 }
 
 bulmaSelectmenu.attachMenu(dimensionBox);
 bulmaSelectmenu.attachMenu(tagsBox);
+
+
+async function openSelectBiomeDlg(transformation) {
+    currentTransformationSelecting = transformation;
+    let tables;
+    openSelectBiomeDlg.classList.add("is-active");
+    let selectBiomeMenu = document.getElementById("selectBiomeMenu");
+    selectBiomeMenu.innerHTML = "";
+    for (let i = 0; i < tables.length; i++) {
+        let selectBiomeMenuItem;
+        let previewBox;
+        let preview;
+        let itemTitle;
+        let itemRadio;
+        selectBiomeMenuItem = document.createElement("div");
+        selectBiomeMenuItem.setAttribute("class", "card textureMenuItem");
+        itemRadio = document.createElement("input");
+        itemRadio.setAttribute("type", "radio");
+        itemRadio.setAttribute("name", "selectedBiome");
+        itemRadio.setAttribute("class", "textureRadio");
+        itemRadio.setAttribute("value", tables[i]);
+        selectBiomeMenuItem.appendChild(itemRadio);
+        itemTitle = document.createElement("span");
+        itemTitle.setAttribute("class", "textureMenuTitle");
+        itemTitle.innerHTML = tables[i];
+        selectBiomeMenuItem.appendChild(itemTitle);
+        selectBiomeMenu.appendChild(selectBiomeMenuItem);
+        selectBiomeMenuItem.addEventListener("click", () => {
+            const itemRadio = selectBiomeMenuItem.querySelector('input[type="radio"]');
+            if (itemRadio) {
+                itemRadio.checked = true;  // select this radio
+            }
+        });
+    }
+}
+function closeSelectBiomeDlg() {
+    selectBiomeDlg.classList.remove("is-active");
+}
+function selectBiome() {
+    const selected = document.querySelector('input[name="selectedBiome"]:checked');
+
+    const textureNameText = document.getElementById("biomeNameText_" + currentTransformationSelecting);
+    if (selected.value == "None") {
+        currentTransformations[currentTransformationSelecting] = "";
+        textureNameText.innerHTML = "No biome selected";
+    } else {
+        currentTransformations[currentTransformationSelecting] = selected.value;
+        textureNameText.innerHTML = selected.value;
+    }
+
+    closeSelectBiomeDlg();
+}

cbfb151d44442460d6f6a02d2494427e5d590cfb
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-02-28 07:39:09 +0900

    bugfix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const appVersion = "2.2.356";
-const buildDate = "2/25/2026";
+const appVersion = "2.2.357";
+const buildDate = "2/28/2026";
 const minEngineVersion = [1, 21, 90];
 const formatVersion = "1.21.90";
 

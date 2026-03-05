726ee326251169c77f7614ce82cb4ad5dc4c9d06
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-03-05 12:04:23 +0900

    bugfix

--- main.js
+++ main.js
@@ -209,7 +209,8 @@ let projectTypes = {
       "1.21.6",
       "1.21.7/1.21.8",
       "1.21.9/1.21.10",
-      "1.21.11"
+      "1.21.11",
+      "26.1"
     ],
     disabled: false,
     exportOptions: [

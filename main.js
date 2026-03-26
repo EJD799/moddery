b3f5eca5d33b9e051df11f2be761905ef8be26e4
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-03-25 22:20:54 -0400

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const appVersion = "2.2.425";
-const buildDate = "3/12/2026";
+const appVersion = "2.3.0";
+const buildDate = "3/26/2026";
 const minEngineVersion = [1, 21, 90];
 const formatVersion = "1.21.90";
 

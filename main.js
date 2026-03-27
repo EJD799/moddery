52bafef88dff27ca611fe1f97c27f09bc188335b
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-03-27 18:46:10 -0400

    bugfix

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const appVersion = "2.3.5";
+const appVersion = "2.3.6";
 const buildDate = "3/28/2026";
 const minEngineVersion = [1, 21, 90];
 const formatVersion = "1.21.90";
@@ -1683,12 +1683,15 @@ if ('launchQueue' in window) {
     if (!launchParams.files.length) return;
 
     try {
-      const fileHandle = launchParams.files[0];
-      projFileHandle = fileHandle;
+      if (storageMode == "local_storage") {
+        alert("Please use file system mode to load projects directly from files!");
+      } else {
+        const fileHandle = launchParams.files[0];
+        projFileHandle = fileHandle;
 
-      const file = await fileHandle.getFile();
-      await openProj(file);
-
+        const file = await fileHandle.getFile();
+        await openProj(file);
+      }
     } catch (err) {
       console.error(err);
       alert("Error opening project from file.");
